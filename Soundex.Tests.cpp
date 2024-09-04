28743e8994a5b9f7d0079943f08afa600e6498e5
sriganesh12 <49653366+sriganesh12@users.noreply.github.com>
2024-09-04 12:04:23 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -10,3 +10,12 @@ TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
 }
 
+TEST(SoundexTest, HandlesMultipleCharacter) {
+    EXPECT_EQ(generateSoundex("ROBERT"), "R163");
+    EXPECT_EQ(generateSoundex("Tymczak"), "T522");
+    EXPECT_EQ(generateSoundex("Pfister"), "P236");
+    EXPECT_EQ(generateSoundex("Ashcraft"), "A261");
+    EXPECT_EQ(generateSoundex("RUBIN"), "R150");
+}
+
+
